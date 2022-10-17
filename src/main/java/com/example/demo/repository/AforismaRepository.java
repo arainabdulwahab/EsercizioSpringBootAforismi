67fdf5458834d561d4cfe5840e517eb459cdd113
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-17 20:52:22 +0200

    aforisma repository

---
+++ src/main/java/com/example/demo/repository/AforismaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Aforisma;
+
+public interface AforismaRepository extends CrudRepository<Aforisma, Integer>{
+	
+}
