6df5bd8af84c71f5ecb5d83d2cef2b9da3e90196
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-17 21:12:41 +0200

    service annotation added

--- src/main/java/com/example/demo/service/AforismaService.java
+++ src/main/java/com/example/demo/service/AforismaService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Aforisma;
 import com.example.demo.repository.AforismaRepository;
 
+@Service
 public class AforismaService  implements IAforismaService{
 
 	@Autowired 
