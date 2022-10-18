e1bb97f3ce7d666ecdb6b855154dcf177fd4ca6f
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-18 11:53:44 +0200

    modified sql result after using postman

--- aforismi.sql
+++ aforismi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2022 at 08:20 PM
+-- Generation Time: Oct 18, 2022 at 11:50 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `aforismi` (
   `id` int(11) NOT NULL,
-  `testo` varchar(1000) NOT NULL
+  `testo` varchar(600) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -37,13 +37,14 @@ CREATE TABLE `aforismi` (
 --
 
 INSERT INTO `aforismi` (`id`, `testo`) VALUES
-(1, 'Ciò che il bruco chiama fine del mondo, il resto del mondo chiama farfalla'),
-(2, 'Ci sono 350 varietà di squali, senza contare gli addetti ai prestiti e ai fondi comuni.'),
-(3, 'Vi sono due cose durevoli che possiamo sperare di lasciare in eredità ai nostri figli: le radici e le ali'),
-(4, 'Quando piove lo stolto impreca contro gli dei, il saggio si procura un ombrello'),
-(28, 'Durante i miei nove anni alle scuole superiori non sono riuscito a insegnare niente ai miei professori.'),
-(36, 'Dietro ogni uomo di successo c\'è una suocera sorpresa.'),
-(37, 'Fa così caldo che il condizionatore mi ha denunciato per sfruttamento.');
+(39, 'il 1 modificato  test '),
+(41, 'il 3 test '),
+(42, 'il 4 test '),
+(43, 'il 5 test '),
+(44, 'il 6 test '),
+(45, 'il 7 test '),
+(46, 'il 8 test '),
+(47, 'last  test ');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +64,7 @@ ALTER TABLE `aforismi`
 -- AUTO_INCREMENT for table `aforismi`
 --
 ALTER TABLE `aforismi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
